9284387675c5af2abaa995f07ba49ebf7a736d31
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-09-09 19:34:12 +0900

    add relax_supernode

---
+++ src/main/relax_supernode_m.f90
@@ -0,0 +1,89 @@
+module relax_supernode_m
+  use jagged_array_m
+  use doubly_linked_lists_m
+  use iterator_m
+  use stack_m
+  implicit none
+  private
+
+  public :: compute_merge_lists
+  
+contains
+  type(doubly_linked_lists_c) function compute_merge_lists(cc, tree_child, first_node, max_zero) result(merge_lists)
+    integer, pointer, contiguous, intent(in) :: cc(:), first_node(:)
+    type(jagged_array_c), intent(in) :: tree_child
+    integer, intent(in) :: max_zero
+    integer :: n, i, j, k, min_zero, child, additional_zero, merge_node
+    integer, pointer, contiguous :: childs(:)
+    integer, allocatable :: num_cols(:), num_zeros(:)
+    type(doubly_linked_lists_c) :: son_lists
+    type(iterator_c) :: iterator
+    type(stack_c) :: stack
+    
+    n = size(cc)
+    son_lists = create_doubly_linked_lists(n)
+    merge_lists = create_doubly_linked_lists(n)
+    do i=1, n
+      call merge_lists%add(i, i)
+    enddo
+
+    allocate(num_cols(n))
+    do i=1, n
+      num_cols(i) = first_node(i+1) - first_node(i)
+    enddo
+    allocate(num_zeros(n))
+    stack = create_stack(n)
+    do k=1, n
+      if(tree_child%get_array_length(k) == 0) cycle
+      childs => tree_child%get_array(k)
+      do j=1, size(childs)
+        call son_lists%add(childs(j), k)
+      enddo
+      do
+        merge_node = choose_merge_node(k, max_zero, son_lists, cc, num_zeros, num_cols, additional_zero)
+        if(merge_node /= 0)then
+          call merge_lists%merge(merge_node, k)
+          call son_lists%remove(merge_node, k)
+          call stack%push(merge_node)
+          num_zeros(k) = additional_zero
+          num_cols(k) = num_cols(k) + num_cols(merge_node)
+        else
+          do while(.not. stack%is_empty())
+            merge_node = stack%pop()
+            call son_lists%merge(merge_node, k)
+          enddo
+          exit
+        endif
+      enddo
+    enddo
+
+
+
+  end function
+
+  integer function choose_merge_node(parent, max_zero, son_lists, cc, num_zeros, num_cols, min_zero) result(merge_node)
+    integer, intent(in) :: parent, max_zero, cc(:), num_zeros(:), num_cols(:)
+    integer, intent(out) :: min_zero
+    type(doubly_linked_lists_c), intent(in) :: son_lists
+    integer :: child, additional_zero
+    type(iterator_c) :: iterator
+
+    min_zero = max_zero
+    merge_node = 0
+    iterator = son_lists%create_iterator(parent)
+    do while(iterator%has_next())
+      child = iterator%next()
+      additional_zero = get_additional_zero(parent, child, cc, num_cols) + num_zeros(parent) + num_zeros(child)
+      if(additional_zero <= min_zero .and. additional_zero <= max_zero)then
+        merge_node = child
+        min_zero = additional_zero
+      endif
+    enddo
+
+  end function
+
+  integer function get_additional_zero(parent, child, cc, ncol) result(additional_zero)
+      integer, intent(in) :: parent, child, cc(:), ncol(:)
+      additional_zero = (cc(parent) + ncol(parent) - cc(child)) * ncol(child)
+    end function
+end module
---
+++ src/test/relax_supernode_test.f90
@@ -0,0 +1,61 @@
+program relax_supernode_test
+  use jagged_array_m
+  use doubly_linked_lists_m
+  use iterator_m
+  use relax_supernode_m
+  use sparse_matrix_maker_m
+  use test_util
+  implicit none
+  
+  type(doubly_linked_lists_c) :: merge_lists
+  type(jagged_array_c) :: tree_child
+  integer, pointer, contiguous :: cc(:), child_ptr(:), child_val(:), first_node(:)
+
+  allocate(cc(7), child_ptr(8), child_val(7), first_node(8))
+  
+  cc = (/2, 2, 2, 2, 2, 2, 0/)
+  child_ptr = (/1, 1, 2, 2, 2, 3, 5, 7/)
+  child_val = (/1, 4, 3, 5, 2, 6/)
+
+  tree_child = create_jagged_array(child_ptr, child_val)
+  first_node = (/1, 2, 3, 5, 6, 7, 8, 10/)
+  merge_lists = compute_merge_lists(cc, tree_child, first_node, 0)
+  call assert_equal("max_zero=0:1", get_merge_nodes(1), (/1/))
+  call assert_equal("max_zero=0:2", get_merge_nodes(2), (/2/))
+  call assert_equal("max_zero=0:3", get_merge_nodes(3), (/3/))
+  call assert_equal("max_zero=0:4", get_merge_nodes(4), (/4/))
+  call assert_equal("max_zero=0:5", get_merge_nodes(5), (/5/))
+  call assert_equal("max_zero=0:7", get_merge_nodes(7), (/7, 6/))
+
+  merge_lists = compute_merge_lists(cc, tree_child, first_node, 1)
+  call assert_equal("max_zero=1:2", get_merge_nodes(2), (/2, 1/))
+  call assert_equal("max_zero=1:3", get_merge_nodes(3), (/3/))
+  call assert_equal("max_zero=1:5", get_merge_nodes(5), (/5, 4/))
+  call assert_equal("max_zero=1:7", get_merge_nodes(7), (/7, 6/))
+  
+  merge_lists = compute_merge_lists(cc, tree_child, first_node, 2)
+  call assert_equal("max_zero=2:5", get_merge_nodes(5), (/5, 4/))
+  call assert_equal("max_zero=2:6", get_merge_nodes(6), (/6, 3/))
+  call assert_equal("max_zero=2:7", get_merge_nodes(7), (/7, 2, 1/))
+
+contains
+  function get_merge_nodes(node) result(merge_nodes)
+    integer, intent(in) :: node
+    type(iterator_c) :: iterator
+    integer, pointer, contiguous :: merge_nodes(:)
+    integer :: ptr
+
+    allocate(merge_nodes(9))
+    iterator = merge_lists%create_iterator(node)
+    ptr = 0
+    do while(iterator%has_next())
+      ptr = ptr + 1
+      merge_nodes(ptr) = iterator%next()
+    enddo
+    
+    merge_nodes => merge_nodes(1:ptr)
+    
+  end function
+  
+  
+end program relax_supernode_test
