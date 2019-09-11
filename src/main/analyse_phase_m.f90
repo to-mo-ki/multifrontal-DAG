ff8528d806a726b768da7ea82134605b6a1d0029
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-09-11 16:15:19 +0900

    refactor analyze_phase

--- src/main/analyse_phase_m.f90
+++ src/main/analyse_phase_m.f90
@@ -6,11 +6,10 @@ module analyze_phase_m
   use tree_m
   use reordering_m
   use perm_m
-  use finding_leaves_m
-  use fundamental_supernode_m
   use relax_supernode_m
   use column_count_m
   use relaxed_supernode_m
+  use fundamental_supernode_m
   implicit none
   private
   
@@ -21,8 +20,7 @@ contains
     integer, intent(in) :: max_zero
     type(jagged_array_c) :: origin_crs, tree_child, postordering_ccs
     integer, pointer, contiguous :: parent(:), postordering_parent(:), postordering_perm(:), postordering_iperm(:)
-    integer, pointer, contiguous :: subtree_size(:), isleaf(:)
-    integer, pointer, contiguous :: first_node(:), last_node(:), num_child_supernode(:), parent_supernode(:)
+    integer, pointer, contiguous :: first_node(:), last_node(:)
     integer, pointer, contiguous :: cc_node(:), cc_supernode(:)
     type(jagged_array_c) :: ccs_supernode, tree_child_supernode, ccs_supernode2, ccs_relaxed
     type(doubly_linked_lists_c) :: merge_lists, son_lists
@@ -30,8 +28,10 @@ contains
     integer :: order
     order = origin_ccs%get_num_arrays()
     call ccs_to_crs(origin_ccs, origin_crs)
+    
     parent => compute_tree(origin_crs)
     tree_child = create_tree_child(parent)
+
     postordering_perm => tree_traverse_postordering(tree_child)
     call set_iperm(postordering_perm, postordering_iperm)
     postordering_ccs = reordering_ccs(origin_ccs, postordering_perm, postordering_iperm)
@@ -40,13 +40,7 @@ contains
     tree_child = create_tree_child(postordering_parent)
     
     ! finding supernode
-    subtree_size => count_subtree_size(tree_child)
-    isleaf => finding_leaves(subtree_size, postordering_ccs)
-    first_node => search_first_node_in_supernode(isleaf, tree_child)
-    ccs_supernode = create_supernodal_ccs(first_node, postordering_ccs)
-    num_child_supernode => create_supernodal_tree(first_node, tree_child)
-    parent_supernode => create_parent_in_postordering_tree(num_child_supernode)
-    tree_child_supernode = create_tree_child(num_child_supernode, parent_supernode)
+    call finding_supernode(tree_child, postordering_ccs, tree_child_supernode, ccs_supernode, first_node)
     ! column count
     cc_node => column_count(postordering_ccs, tree_child, postordering_parent)
     cc_supernode => create_supernodal_column_count(first_node, cc_node)
@@ -56,6 +50,7 @@ contains
     map => build_map(merge_lists)
     first_node_relaxed => create_first_node(first_node, map, merge_lists)
     ! TODO: ccs_supernode2, map2, merge_lists2 by reordering ccs_supernode
+
     ccs_relaxed = create_ccs(map, merge_lists, first_node_relaxed, ccs_supernode2, order)
     ! symbolic
 
@@ -65,5 +60,23 @@ contains
     
     
 
+  end subroutine
+
+  subroutine finding_supernode(tree_child_node, ccs_node, tree_child_supernode, ccs_supernode, first_node)
+    use finding_leaves_m
+    use fundamental_supernode_m
+    type(jagged_array_c), intent(in) :: tree_child_node, ccs_node
+    type(jagged_array_c), intent(out) :: tree_child_supernode, ccs_supernode
+    integer, pointer, contiguous, intent(out) :: first_node(:)
+    integer, pointer, contiguous :: subtree_size(:), isleaf(:), num_child_supernode(:), parent_supernode(:)
+
+    subtree_size => count_subtree_size(tree_child_node)
+    isleaf => finding_leaves(subtree_size, ccs_node)
+    first_node => search_first_node_in_supernode(isleaf, tree_child_node)
+    ccs_supernode = create_supernodal_ccs(first_node, ccs_node)
+    num_child_supernode => create_supernodal_tree(first_node, tree_child_node)
+    parent_supernode => create_parent_in_postordering_tree(num_child_supernode)
+    tree_child_supernode = create_tree_child(num_child_supernode, parent_supernode)
+   
   end subroutine
 end module
