dd39c48b741d00f3db1c6f1f09b44f1eeb3cc78b
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-09-15 11:29:38 +0900

    add perm

--- src/main/analyse_phase_m.f90
+++ src/main/analyse_phase_m.f90
@@ -17,15 +17,17 @@ module analyze_phase_m
   private
   
 contains
-  subroutine analyze_phase(origin_ccs, max_zero)
+  subroutine analyze_phase(origin_ccs, max_zero, perm_out)
     ! NOTE: reorderingは外部で行う
     type(jagged_array_c), pointer, intent(in) :: origin_ccs
     integer, intent(in) :: max_zero
-    type(jagged_array_c), pointer :: origin_crs, tree_child, ccs_l, ccs_supernode
+    type(jagged_array_c), pointer :: origin_crs, tree_child, ccs_l, ccs_supernode, postordering_ccs
     integer, pointer, contiguous :: parent(:), postordering_parent(:)
     integer, pointer, contiguous :: cc_node(:), postordering_perm(:), postordering_iperm(:)
     type(supernode_c), pointer :: fundamental, relaxed
+    integer, pointer, contiguous :: perm_out(:)
     
+    allocate(fundamental, relaxed)
     call ccs_to_crs(origin_ccs, origin_crs)
     
     parent => compute_tree(origin_crs)
@@ -33,15 +35,15 @@ contains
 
     postordering_perm => tree_traverse_postordering(tree_child)
     call set_iperm(postordering_perm, postordering_iperm)
-    fundamental%ccs = reordering_ccs(origin_ccs, postordering_perm, postordering_iperm)
+    postordering_ccs => reordering_ccs(origin_ccs, postordering_perm, postordering_iperm)
     postordering_parent => reordering_tree(parent, postordering_perm, postordering_iperm)
     ! TODO: finalize tree_child
     tree_child => create_tree_child(postordering_parent)
     ! column count
-    cc_node => column_count(fundamental%ccs, tree_child, postordering_parent)
+    cc_node => column_count(postordering_ccs, tree_child, postordering_parent)
     
     ! finding supernode
-    ! TODO: fundamental_ccsの分離
+    fundamental%ccs => postordering_ccs
     fundamental%perm => postordering_perm
     fundamental%iperm => postordering_iperm
     call finding_supernode(tree_child, cc_node, fundamental)
@@ -52,12 +54,13 @@ contains
     
     ccs_supernode => create_supernodal_ccs(relaxed%node_sets, relaxed%ccs)
 
-    ! symbolic
-    ! TODO: cc_relaxed, tree_childを求める
-    ccs_l = symbolic_factorize(ccs_supernode, relaxed%node_sets, relaxed%cc, relaxed%tree_child)
+    ccs_l => symbolic_factorize(ccs_supernode, relaxed%node_sets, relaxed%cc, relaxed%tree_child)
 
+    ! Tree pruning
     ! local index
-    ! reordering-ccs_value
+    ! CCSのオーダリングはpermを用いて行う(ccs_permを作成しない)
+    call perm_product(fundamental%perm, relaxed%perm, perm_out)
+
 
     
     
@@ -74,7 +77,7 @@ contains
 
     subtree_size => count_subtree_size(tree_child)
     isleaf => finding_leaves(subtree_size, fundamental%ccs)
-    fundamental%node_sets = search_node_sets_in_supernode(isleaf, tree_child)
+    fundamental%node_sets => search_node_sets_in_supernode(isleaf, tree_child)
     num_child_supernode => create_supernodal_tree(fundamental%node_sets, tree_child)
     parent_supernode => create_parent_in_postordering_tree(num_child_supernode)
     fundamental%tree_child => create_tree_child(num_child_supernode, parent_supernode)
@@ -83,13 +86,13 @@ contains
   end subroutine
 
   subroutine relax_supernode(fundamental, relaxed, max_zero)
-    type(supernode_c), pointer, intent(in) :: fundamental
-    type(supernode_c), pointer, intent(out) :: relaxed
+    type(supernode_c), intent(in) :: fundamental
+    type(supernode_c), intent(out) :: relaxed
     integer, intent(in) :: max_zero
     type(doubly_linked_lists_c), pointer :: merge_lists, sons
     integer, pointer, contiguous :: map(:), num_child(:), parent(:)
 
-    merge_lists = compute_merge_lists(fundamental%cc, fundamental%tree_child, fundamental%node_sets, max_zero, sons)
+    merge_lists => compute_merge_lists(fundamental%cc, fundamental%tree_child, fundamental%node_sets, max_zero, sons)
     map => build_map(merge_lists)
     relaxed%node_sets => create_node_sets(fundamental%node_sets, map, merge_lists)
     relaxed%perm => create_perm(fundamental%node_sets, relaxed%node_sets, map, merge_lists)
