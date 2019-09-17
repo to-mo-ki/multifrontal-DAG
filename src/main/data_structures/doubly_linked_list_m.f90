c15a7d9c3e7010d20fbb410a2246937c580965e6
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-09-17 12:19:19 +0900

    add doubly_linked_list

---
+++ src/main/data_structures/doubly_linked_list_m.f90
@@ -0,0 +1,89 @@
+module doubly_linked_list_m
+  use iterator_m, new_iterator => create_iterator
+  implicit none
+  private
+  type, public :: doubly_linked_list_c
+    private
+    integer :: head
+    integer, pointer:: next(:), prev(:)
+  contains
+    procedure :: add
+    procedure :: remove
+    procedure :: get_length
+    procedure :: create_iterator
+  end type
+
+  public :: create_doubly_linked_list
+
+contains
+  function create_doubly_linked_list(n) result(this)
+    type(doubly_linked_list_c), pointer :: this
+    integer,intent(in) :: n
+
+    allocate(this)
+    allocate(this%next(n), this%prev(n))
+    this%next = 0
+    this%prev = 0
+    this%head = 0
+
+  end function
+
+  subroutine add(this, node)
+    class(doubly_linked_list_c) :: this
+    integer, intent(in) :: node
+
+    if(this%head /= 0)then
+      this%prev(this%head) = node
+    endif
+    this%next(node) = this%head
+    this%head = node
+    this%prev(node) = 0
+    
+  end subroutine add
+
+  subroutine remove(this, node)
+    class(doubly_linked_list_c) :: this
+    integer, intent(in) :: node
+    integer :: next_node, prev_node
+
+    next_node = this%next(node)
+    prev_node = this%prev(node)
+    
+    if(prev_node == 0)then
+      this%head = next_node
+    else
+      this%next(prev_node) = next_node
+    endif
+    if(next_node /= 0)then
+      this%prev(next_node) = prev_node
+    endif
+    
+    this%next(node) = 0
+    this%prev(node) = 0
+
+  end subroutine remove
+
+  integer function get_length(this) result(length)
+    class(doubly_linked_list_c) :: this
+    type(iterator_c), pointer :: iterator
+    integer :: tmp
+
+    length = 0
+    iterator => this%create_iterator()
+    do while(iterator%has_next())
+      length = length + 1
+      tmp = iterator%next()
+    end do
+
+  end function
+
+  function create_iterator(this) result(iterator)
+    type(iterator_c), pointer :: iterator
+    class(doubly_linked_list_c) :: this
+
+    allocate(iterator)
+    iterator => new_iterator(this%head, this%next)
+
+  end function
+
+end module
---
+++ src/test/data_structures/doubly_linked_list_test.f90
@@ -0,0 +1,43 @@
+program doubly_linked_list_test
+  use doubly_linked_list_m
+  use iterator_m
+  use test_util
+  implicit none
+  type(doubly_linked_list_c), pointer :: list
+  type(iterator_c), pointer :: iterator
+  integer :: n
+
+  n = 5
+  list => create_doubly_linked_list(n)
+  call list%add(2)
+  call check_list("add 2", (/2/))
+  call list%add(5)
+  call check_list("add 5", (/5, 2/))
+  call list%add(3)
+  call check_list("add 3", (/3, 5, 2/))
+  call list%remove(3)
+  call check_list("remove 3", (/5, 2/))
+  call list%remove(2)
+  call check_list("remove 2", (/5/))
+  call list%remove(5)
+  call assert_equal("remove 5", list%get_length(), 0)
+  
+  
+contains
+  subroutine check_list(message, check)
+    character(len=*), intent(in) :: message
+    integer, intent(in) :: check(:)
+    integer :: i, val(5)
+
+    iterator => list%create_iterator()
+    i=0
+    do while(iterator%has_next())
+      i = i + 1
+      val(i) = iterator%next()
+    enddo
+    call assert_equal(message, val(:i), check)
+
+  end subroutine
+
+  
+end program doubly_linked_list_test
