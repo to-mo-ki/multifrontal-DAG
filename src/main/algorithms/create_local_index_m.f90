ede77eb08de6a01b4a59584144682e25253c171f
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-11-06 17:56:44 +0900

    generate_local_index => create_local_index

---
+++ src/main/algorithms/create_local_index_m.f90
@@ -0,0 +1,47 @@
+module create_local_index_m
+  use jagged_array_m
+  use contiguous_sets_m
+  implicit none
+  private
+
+  public :: create_local_index
+
+contains
+  function create_local_index(ccs, node_sets, tree_child) result(local_index)
+    type(jagged_array_c), pointer :: ccs, tree_child
+    type(contiguous_sets_c), pointer :: node_sets
+    type(jagged_array_c), pointer :: local_index
+    type(contiguous_sets_c), pointer :: ptr
+    integer :: node, num_col, offset, child, i, j
+    integer, pointer, contiguous :: rows(:), childs(:), local(:)
+    integer, allocatable :: map(:)
+    
+    allocate(local_index)
+    allocate(map(node_sets%get_num_elements()))
+    ptr => ccs%get_set()
+    local_index => create_jagged_array(ptr)
+    do node=1, ccs%get_num_arrays()
+      if(tree_child%get_array_length(k) == 0) cycle
+      num_col = node_sets%get_length(node)
+      offset = node_sets%get_first(node)-1
+      do i=1, num_col
+        map(offset+i) = i
+      enddo
+      rows => ccs%get_array(node)
+      do i=1, size(rows)
+        map(rows(i)) = num_col + i
+      enddo
+      childs => tree_child%get_array(node)
+      do j=1, size(childs)
+        child = childs(j)
+        local => local_index%get_array(child)
+        rows => ccs%get_array(child)
+        do i=1, size(local)
+          local(i) = map(rows(i))
+        enddo
+      enddo
+    enddo
+
+  end function
+  
+end module
---
+++ src/test/algorithms/create_local_index_test.f90
@@ -0,0 +1,25 @@
+program create_local_index_test
+  use create_local_index_m
+  use sparse_matrix_maker_m
+  use tree_maker_m
+  use test_util
+  use jagged_array_m
+  use contiguous_sets_m
+  implicit none
+  type(jagged_array_c), pointer :: ccs, local_index, tree_child
+  type(contiguous_sets_c), pointer :: node_sets
+  integer, pointer, contiguous :: num_row(:), row(:), node_size(:)
+  integer, pointer, contiguous :: num_child(:), child_val(:)
+
+  call make_supernodal_ccs(num_row, row, node_size)
+  ccs => create_jagged_array(num_row, row)
+  node_sets => create_contiguous_sets(node_size)
+  call make_supernodal_tree(num_child, child_val)
+  tree_child => create_jagged_array(num_child, child_val)
+
+  local_index => create_local_index(ccs, node_sets, tree_child)
+  call assert_equal("local index", local_index%get_val(), (/1, 2, 1, 2, 1, 3, 1, 3, 1, 2, 1, 2/))
+
+
+  
+end program create_local_index_test
