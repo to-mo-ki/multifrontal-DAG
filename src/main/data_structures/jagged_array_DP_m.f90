d24aa18ff0f9e3c71482419ecbf488d256ba5154
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-11-06 12:34:58 +0900

    add jagged_array_DP

---
+++ src/main/data_structures/jagged_array_DP_m.f90
@@ -0,0 +1,34 @@
+module jagged_array_DP_m
+  use contiguous_sets_m
+  implicit none
+  private
+  type, public :: jagged_array_DP_c
+    private
+    type(contiguous_sets_c), pointer :: set
+    double precision, pointer, contiguous :: val(:)
+  contains
+    procedure :: get_array
+  end type
+
+  public :: create_jagged_array_DP
+  
+contains
+  function create_jagged_array_DP(set, val) result(this)
+    type(jagged_array_DP_c), pointer :: this
+    type(contiguous_sets_c), pointer :: set
+    double precision, pointer, contiguous :: val(:)
+
+    allocate(this)
+    this%set => set
+    this%val => val
+
+  end function
+
+  function get_array(this, idx) result(ptr)
+    class(jagged_array_DP_c), target :: this
+    integer, intent(in) :: idx
+    double precision, pointer, contiguous :: ptr(:)
+    ptr => this%val(this%set%get_first(idx):this%set%get_last(idx))
+  end function
+
+end module
---
+++ src/test/data_structures/jagged_array_DP_test.f90
@@ -0,0 +1,30 @@
+program jagged_array_test
+  use sparse_matrix_maker_m
+  use test_util
+  use jagged_array_DP_m
+  use contiguous_sets_m
+  implicit none
+  type(jagged_array_DP_c), pointer :: ccs
+  type(contiguous_sets_c), pointer :: set
+  integer, pointer, contiguous :: col(:), row(:)
+  double precision, pointer, contiguous :: row_val(:)
+  integer :: i
+  call make_ccs(col, row)
+  allocate(row_val(size(row)))
+  do i=1, size(row)
+    row_val(i) = dble(row(i))
+  enddo
+  set => create_contiguous_sets(col)
+  ccs => create_jagged_array_DP(set, row_val)
+
+  call assert_equal("get_array(1)", ccs%get_array(1), (/1d0, 7d0, 8d0/))
+  call assert_equal("get_array(2)", ccs%get_array(2), (/2d0, 4d0, 6d0, 9d0/))
+  call assert_equal("get_array(3)", ccs%get_array(3), (/3d0, 5d0, 8d0/))
+  call assert_equal("get_array(4)", ccs%get_array(4), (/4d0, 9d0/))
+  call assert_equal("get_array(5)", ccs%get_array(5), (/5d0, 6d0, 8d0/))
+  call assert_equal("get_array(6)", ccs%get_array(6), (/6d0, 9d0/))
+  call assert_equal("get_array(7)", ccs%get_array(7), (/7d0, 9d0/))
+  call assert_equal("get_array(8)", ccs%get_array(8), (/8d0/))
+  call assert_equal("get_array(9)", ccs%get_array(9), (/9d0/))
+  
+end program
