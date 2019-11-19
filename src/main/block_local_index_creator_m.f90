12c22348a28896bb0b0e0d21ff0fbb3716775f6b
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-11-19 12:28:28 +0900

    modify block_local_index_creator

--- src/main/block_local_index_creator_m.f90
+++ src/main/block_local_index_creator_m.f90
@@ -1,6 +1,7 @@
 module block_local_index_creator_m
   use contiguous_sets_m
   use jagged_array_m
+  use jagged_array_3D_m
   use node_data_m
   implicit none
   private
@@ -8,20 +9,19 @@ module block_local_index_creator_m
   public :: create_parent_ptr, create_parent_block_num, create_parent_block_ptr, rebuild_val
 contains
 
-  function create_parent_ptr(node_data, local_index) result(parent_ptr)
+  function create_parent_ptr(local_index, nb, max_num_block) result(parent_ptr)
     type(node_data_c), pointer :: node_data
     type(jagged_array_c), pointer :: local_index
     type(contiguous_sets_c), pointer :: parent_ptr
-    integer:: nb, num_node
+    integer, intent(in) :: nb, max_num_block
     integer, pointer, contiguous :: local(:)
-    integer :: node, i, block_num, wsize
+    integer :: num_node, node, i, block_num, wsize
     integer, allocatable :: marker(:)
     integer, allocatable :: lengths(:)
 
-    nb = node_data%nb
-    num_node = node_data%num_node
+    num_node = local_index%get_num_arrays()
 
-    allocate(marker(node_data%max_num_block), lengths(num_node))
+    allocate(marker(max_num_block), lengths(num_node))
     marker = 0
     lengths = 0
     do node=1, num_node-1
@@ -39,21 +39,19 @@ contains
 
   end function
 
-  function create_parent_block_num(node_data, local_index, set) result(parent_block_num)
-    type(node_data_c), pointer :: node_data
+  function create_parent_block_num(local_index, set, nb, max_num_block) result(parent_block_num)
     type(jagged_array_c), pointer :: local_index
     type(contiguous_sets_c), pointer :: set
-    integer :: nb, num_node
+    integer, intent(in) :: nb, max_num_block
     type(jagged_array_c), pointer :: parent_block_num
     integer, pointer, contiguous :: local(:), plocal(:)
-    integer :: node, i, block_num, wsize, ptr
+    integer :: num_node, node, i, block_num, wsize, ptr
     integer, allocatable :: marker(:)
 
-    nb = node_data%nb
-    num_node = node_data%num_node
+    num_node = local_index%get_num_arrays()
 
     parent_block_num => create_jagged_array(set)
-    allocate(marker(node_data%max_num_block))
+    allocate(marker(max_num_block))
     marker = 0
     do node=1, num_node-1
       local => local_index%get_array(node)
@@ -71,21 +69,19 @@ contains
 
   end function
 
-  function create_parent_block_ptr(node_data, local_index, set) result(parent_block_ptr)
-    type(node_data_c), pointer :: node_data
+  function create_parent_block_ptr(local_index, set, nb, max_num_block) result(parent_block_ptr)
     type(jagged_array_c), pointer :: local_index
     type(contiguous_sets_c), pointer :: set
-    integer :: nb, num_node
+    integer, intent(in) :: nb, max_num_block
     type(jagged_array_c), pointer :: parent_block_ptr
     integer, pointer, contiguous :: local(:), plocal(:)
-    integer :: node, i, block_num, wsize, offset, ptr
+    integer :: num_node, node, i, block_num, wsize, offset, ptr
     integer, allocatable :: marker(:)
 
-    nb = node_data%nb
-    num_node = node_data%num_node
+    num_node = local_index%get_num_arrays()
 
     parent_block_ptr => create_jagged_array(set)
-    allocate(marker(node_data%max_num_block))
+    allocate(marker(max_num_block))
 
     marker = 0
     ptr = 0
@@ -107,13 +103,11 @@ contains
 
   end function
 
-  subroutine rebuild_val(local_index, parent_block_ptr, parent_block_num, num_node, nb)
+  subroutine rebuild_val(local_index, nb)
     integer, contiguous, intent(inout) :: local_index(:)
-    type(jagged_array_c), pointer :: parent_block_ptr, parent_block_num
-    integer, intent(in) :: num_node, nb
-    integer :: node, i, j
-    integer, pointer, contiguous :: ptr(:), num(:)
-
+    integer, intent(in) :: nb
+    integer :: i
+    
     do i=1, size(local_index)
       local_index(i) = mod(local_index(i), nb)
       if(local_index(i) == 0)then
--- src/test/block_local_index_creator_test.f90
+++ src/test/block_local_index_creator_test.f90
@@ -1,6 +1,7 @@
 program block_local_index_creator_test
   use contiguous_sets_m
   use jagged_array_m
+  use jagged_array_3D_m
   use block_local_index_creator_m
   use node_data_m
   use test_util
@@ -9,6 +10,7 @@ program block_local_index_creator_test
   type(node_data_c), pointer :: node_data
   type(jagged_array_c), pointer :: parent_block_num, parent_block_ptr
   type(contiguous_sets_c), pointer :: parent_ptr
+  type(jagged_array_3D_c), pointer :: block_local_index
   integer, pointer, contiguous :: local_index_val(:)
 
   call test1
@@ -18,75 +20,72 @@ program block_local_index_creator_test
 contains
   subroutine test1()
     local_index => create_jagged_array((/4, 0/), (/2, 3, 7, 8/))
-    node_data => create_node_data((/5, 8/), (/8, 0/), 3)
-
-    parent_ptr => create_parent_ptr(node_data, local_index)
+    
+    parent_ptr => create_parent_ptr(local_index, 3, 3)
     call assert_equal("parent_ptr:1", parent_ptr%get_length(1), 2)
 
-    parent_block_num => create_parent_block_num(node_data, local_index, parent_ptr)
+    parent_block_num => create_parent_block_num(local_index, parent_ptr, 3, 3)
     call assert_equal("parent_block_num", parent_block_num%get_array(1), (/1, 3/))
 
-    parent_block_ptr => create_parent_block_ptr(node_data, local_index, parent_ptr)
+    parent_block_ptr => create_parent_block_ptr(local_index, parent_ptr, 3, 3)
     call assert_equal("parent_block_ptr", parent_block_ptr%get_array(1), (/1, 3/))
 
     local_index_val => local_index%get_raw_val()
-    call rebuild_val(local_index_val, parent_block_ptr, parent_block_num, 2, 3)
+    call rebuild_val(local_index_val, 3)
     call assert_equal("rebuild_val", local_index_val, (/2, 3, 1, 2/))
 
   end subroutine
 
   subroutine test2()
     local_index => create_jagged_array((/3, 2, 2, 1, 0/), (/2, 4, 6, 1, 5, 2, 3, 1/))
-    node_data => create_node_data((/3, 2, 4, 1, 3/), (/3, 2, 2, 1, 0/), 2)
 
-    parent_ptr => create_parent_ptr(node_data, local_index)
+    parent_ptr => create_parent_ptr(local_index, 2, 3)
     call assert_equal("parent_ptr:1", parent_ptr%get_length(1), 3)
     call assert_equal("parent_ptr:2", parent_ptr%get_length(2), 2)
     call assert_equal("parent_ptr:3", parent_ptr%get_length(3), 2)
     call assert_equal("parent_ptr:4", parent_ptr%get_length(4), 1)
 
-    parent_block_num => create_parent_block_num(node_data, local_index, parent_ptr)
+    parent_block_num => create_parent_block_num(local_index, parent_ptr, 2, 3)
     call assert_equal("parent_block_num:1", parent_block_num%get_array(1), (/1, 2, 3/))
     call assert_equal("parent_block_num:2", parent_block_num%get_array(2), (/1, 3/))
     call assert_equal("parent_block_num:3", parent_block_num%get_array(3), (/1, 2/))
     call assert_equal("parent_block_num:4", parent_block_num%get_array(4), (/1/))
 
-    parent_block_ptr => create_parent_block_ptr(node_data, local_index, parent_ptr)
+    parent_block_ptr => create_parent_block_ptr(local_index, parent_ptr, 2, 3)
     call assert_equal("parent_block_ptr:1", parent_block_ptr%get_array(1), (/1, 2, 3/))
     call assert_equal("parent_block_ptr:2", parent_block_ptr%get_array(2), (/4, 5/))
     call assert_equal("parent_block_ptr:3", parent_block_ptr%get_array(3), (/6, 7/))
     call assert_equal("parent_block_ptr:4", parent_block_ptr%get_array(4), (/8/))
 
     local_index_val => local_index%get_raw_val()
-    call rebuild_val(local_index_val, parent_block_ptr, parent_block_num, 5, 2)
+    call rebuild_val(local_index_val, 2)
     call assert_equal("rebuild_val", local_index_val, (/2, 2, 2, 1, 1, 2, 1, 1/))
 
   end subroutine
 
   subroutine test3()
     local_index => create_jagged_array((/3, 2, 3, 1, 0/), (/2, 4, 7, 1, 6, 1, 2, 3, 1/))
-    node_data => create_node_data((/3, 2, 4, 1, 3/), (/3, 2, 3, 1, 0/), 2)
 
-    parent_ptr => create_parent_ptr(node_data, local_index)
+    parent_ptr => create_parent_ptr(local_index, 2, 4)
     call assert_equal("parent_ptr:1", parent_ptr%get_length(1), 3)
     call assert_equal("parent_ptr:2", parent_ptr%get_length(2), 2)
     call assert_equal("parent_ptr:3", parent_ptr%get_length(3), 2)
     call assert_equal("parent_ptr:4", parent_ptr%get_length(4), 1)
 
-    parent_block_num => create_parent_block_num(node_data, local_index, parent_ptr)
+    parent_block_num => create_parent_block_num(local_index, parent_ptr, 2, 4)
     call assert_equal("parent_block_num:1", parent_block_num%get_array(1), (/1, 2, 4/))
     call assert_equal("parent_block_num:2", parent_block_num%get_array(2), (/1, 3/))
     call assert_equal("parent_block_num:3", parent_block_num%get_array(3), (/1, 2/))
     call assert_equal("parent_block_num:4", parent_block_num%get_array(4), (/1/))
 
-    parent_block_ptr => create_parent_block_ptr(node_data, local_index, parent_ptr)
+    parent_block_ptr => create_parent_block_ptr(local_index, parent_ptr, 2, 4)
     call assert_equal("parent_block_ptr:1", parent_block_ptr%get_array(1), (/1, 2, 3/))
     call assert_equal("parent_block_ptr:2", parent_block_ptr%get_array(2), (/4, 5/))
     call assert_equal("parent_block_ptr:3", parent_block_ptr%get_array(3), (/6, 8/))
     call assert_equal("parent_block_ptr:4", parent_block_ptr%get_array(4), (/9/))
 
     local_index_val => local_index%get_raw_val()
-    call rebuild_val(local_index_val, parent_block_ptr, parent_block_num, 5, 2)
+    call rebuild_val(local_index_val, 2)
     call assert_equal("rebuild_val", local_index_val, (/2, 2, 1, 1, 2, 1, 2, 1, 1/))
 
   end subroutine
