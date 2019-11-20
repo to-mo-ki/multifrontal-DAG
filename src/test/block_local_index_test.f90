46806e051e6cd18ff6b81f259e47993a2f178a37
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-11-20 16:21:11 +0900

    add test in block_local_index_test

--- src/test/block_local_index_test.f90
+++ src/test/block_local_index_test.f90
@@ -12,6 +12,7 @@ program block_local_index_test
   call test1
   call test2
   call test3
+  call test4
 
 contains
   subroutine test1()
@@ -75,6 +76,26 @@ contains
     call assert_equal("local_index:node=3, block_num=1", block_local_index%get_local_index(3, 1), [1, 2])
     call assert_equal("local_index:node=3, block_num=2", block_local_index%get_local_index(3, 2), [1])
     call assert_equal("local_index:node=4, block_num=1", block_local_index%get_local_index(4, 1), [1])
+    
+  end subroutine
+
+
+  subroutine test4()
+    local_index => create_jagged_array((/5, 2, 0/), (/2, 3, 4, 5, 7, 3, 4/))
+    node_set => create_contiguous_sets((/4, 5, 4/))
+    nb = 3
+    block_local_index => create_block_local_index(node_set, local_index, nb)
+    
+    call assert_equal("block_nums:node=1", block_local_index%get_block_nums(1), [1, 2, 3])
+    call assert_equal("block_nums:node=2", block_local_index%get_block_nums(2), [1, 2])
+    call assert_equal("over:node=1", block_local_index%get_overs(1), [0, -1, 0])
+    call assert_equal("over:node=2", block_local_index%get_overs(2), [0, -1])
+
+    call assert_equal("local_index:node=1, block_num=1", block_local_index%get_local_index(1, 1), [2, 3])
+    call assert_equal("local_index:node=1, block_num=2", block_local_index%get_local_index(1, 2), [1, 2])
+    call assert_equal("local_index:node=1, block_num=3", block_local_index%get_local_index(1, 3), [1])
+    call assert_equal("local_index:node=2, block_num=1", block_local_index%get_local_index(2, 1), [3])
+    call assert_equal("local_index:node=2, block_num=2", block_local_index%get_local_index(2, 2), [1])
     
   end subroutine
 
