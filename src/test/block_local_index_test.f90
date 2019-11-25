4e6472ae91bc553c8640612dfb63958307b411bb
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-11-25 13:55:58 +0900

    add extend_add_subroutines

--- src/test/block_local_index_test.f90
+++ src/test/block_local_index_test.f90
@@ -81,6 +81,7 @@ contains
 
 
   subroutine test4()
+    ! XXX:本当はlocal_index => create_jagged_array((/5, 3, 0/), (/2, 3, 4, 5, 7, 3, 4/))
     local_index => create_jagged_array((/5, 2, 0/), (/2, 3, 4, 5, 7, 3, 4/))
     node_set => create_contiguous_sets((/4, 5, 4/))
     nb = 3
