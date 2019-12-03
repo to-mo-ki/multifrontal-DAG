28f83f32fb393680c35137601af33169bfe3139f
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-03 10:15:31 +0900

    refactor border_controller_test

--- src/test/block_matrices/border_controller_test.f90
+++ src/test/block_matrices/border_controller_test.f90
@@ -1,57 +1,74 @@
 program supernode_controller_test
   use controller_test_routines_m, parent_check_estimate => check_estimate
   use controller_test_routines_m, parent_check_ptr => check_ptr
+  use test_util
   use matrix_controller_m
   use border_controller_m
   implicit none
+  class(matrix_controller_c), pointer :: controller
+  integer :: array_size, i
+  double precision, pointer, contiguous :: array(:)
 
-  call check_estimate(3, 5, 5, 21)
-  call check_ptr(3, 5, 5, 2, 2, 1, 9)
-  call check_ptr(3, 5, 5, 3, 2, 10, 9)
-  call check_ptr(3, 5, 5, 4, 2, 19, 3)
+  allocate(border_controller_c::controller)
 
-  call check_estimate(3, 6, 4, 0)
+  print *, "nb=3, nc=5, nr=5"
+  array_size = controller%estimate_size(3, 5, 5)  
+  call assert_equal("estimate_size", array_size, 21)
+  allocate(array(array_size))
+  array = [(dble(i), i=1,array_size)]
+  call start_tests("get_ptr")
+  call add_test("(i, j)=(2,2)", controller%get_ptr(array, 3, 5, 5, 2, 2), [(dble(i), i=1,9)])
+  call add_test("(i, j)=(3,2)", controller%get_ptr(array, 3, 5, 5, 3, 2), [(dble(i), i=10,18)])
+  call add_test("(i, j)=(4,2)", controller%get_ptr(array, 3, 5, 5, 4, 2), [(dble(i), i=19,21)])
+  call end_tests()
 
-  call check_estimate(3, 7, 4, 15)
-  call check_ptr(3, 7, 4, 3, 3, 1, 9)
-  call check_ptr(3, 7, 4, 4, 3, 10, 6)
+  print *, "nb=3, nc=6, nr=4"
+  array_size = controller%estimate_size(3, 6, 4)  
+  call assert_equal("estimate_size", array_size, 0)
 
-  call check_estimate(3, 5, 4, 18)
-  call check_ptr(3, 5, 4, 2, 2, 1, 9)
-  call check_ptr(3, 5, 4, 3, 2, 10, 9)
+  print *, "nb=3, nc=7, nr=4"
+  array_size = controller%estimate_size(3, 7, 4)  
+  call assert_equal("estimate_size", array_size, 15)
+  allocate(array(array_size))
+  array = [(dble(i), i=1,array_size)]
+  call start_array_tests("get_ptr")
+  call add_test("(i, j)=(3,3)", controller%get_ptr(array, 3, 7, 4, 3, 3), [(dble(i), i=1,9)])
+  call add_test("(i, j)=(4,3)", controller%get_ptr(array, 3, 7, 4, 4, 3), [(dble(i), i=10,15)])
+  call end_array_tests()
 
-  call check_estimate(3, 3, 6, 0)
+  print *, "nb=3, nc=5, nr=4"
+  array_size = controller%estimate_size(3, 5, 4)  
+  call assert_equal("estimate_size", array_size, 18)
+  allocate(array(array_size))
+  array = [(dble(i), i=1,array_size)]
+  call start_array_tests("get_ptr")
+  call add_test("(i, j)=(2,2)", controller%get_ptr(array, 3, 5, 4, 2, 2), [(dble(i), i=1,9)])
+  call add_test("(i, j)=(3,2)", controller%get_ptr(array, 3, 5, 4, 3, 2), [(dble(i), i=10,18)])
+  call end_array_tests()
 
-  call check_estimate(4, 2, 7, 36)
-  call check_ptr(4, 2, 7, 1, 1, 1, 16)
-  call check_ptr(4, 2, 7, 2, 1, 17, 16)
-  call check_ptr(4, 2, 7, 3, 1, 33, 4)
+  print *, "nb=3, nc=3, nr=6"
+  array_size = controller%estimate_size(3, 3, 6)  
+  call assert_equal("estimate_size", array_size, 0)
 
-  ! NOTE: nr < nb
-  call check_estimate(4, 5, 2, 9)
-  call check_ptr(4, 5, 2, 2, 2, 1, 9)
-  
-contains
-  
-  subroutine check_estimate(nb, nc, nr, ans_size)
-    integer, intent(in) :: nb, nc, nr
-    integer, intent(in) :: ans_size
-    class(matrix_controller_c), pointer :: controller
-    
-    allocate(border_controller_c::controller)
-    call parent_check_estimate(controller, nb, nc, nr, ans_size)
+  print *, "nb=4, nc=2, nr=7"
+  array_size = controller%estimate_size(4, 2, 7)
+  call assert_equal("estimate_size", array_size, 36)
+  allocate(array(array_size))
+  array = [(dble(i), i=1,array_size)]
+  call start_array_tests("get_ptr")
+  call add_test("(i, j)=(1,1)", controller%get_ptr(array, 4, 2, 7, 1, 1), [(dble(i), i=1,16)])
+  call add_test("(i, j)=(2,1)", controller%get_ptr(array, 4, 2, 7, 2, 1), [(dble(i), i=17,32)])
+  call add_test("(i, j)=(3,1)", controller%get_ptr(array, 4, 2, 7, 3, 1), [(dble(i), i=33,36)])
+  call end_array_tests()
 
-  end subroutine
-
-  subroutine check_ptr(nb, nc, nr, i, j, ans_pos, ans_size)
-    integer, intent(in) :: nb, nc, nr, i, j
-    integer, intent(in) :: ans_pos, ans_size
-    class(matrix_controller_c), pointer :: controller
-    
-    allocate(border_controller_c::controller)
-    call parent_check_ptr(controller, nb, nc, nr, i, j, ans_pos, ans_size)
-
-  end subroutine
+  print *, "nb=3, nc=5, nr=5(nr < nb)"
+  array_size = controller%estimate_size(4, 5, 2)  
+  call assert_equal("estimate_size", array_size, 9)
+  allocate(array(array_size))
+  array = [(dble(i), i=1,array_size)]
+  call start_tests("get_ptr")
+  call add_test("(i, j)=(2,2)", controller%get_ptr(array, 4, 5, 2, 2, 2), [(dble(i), i=1,9)])
+  call end_tests()
   
 end program supernode_controller_test
 
