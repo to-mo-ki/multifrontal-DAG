6f5a49e35c31c54791cf3c7673473753b472d002
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-03 09:48:07 +0900

    refactor contiguous_sets_test

--- src/test/data_structures/contiguous_sets_test.f90
+++ src/test/data_structures/contiguous_sets_test.f90
@@ -12,16 +12,25 @@ program contiguous_sets_test
 
   call assert_equal("num sets", set%get_num_sets(), 4)
   call assert_equal("num elements", set%get_num_elements(), 6)
-  call assert_equal("first:1", set%get_first(1), 1)
-  call assert_equal("first:2", set%get_first(2), 3)
-  call assert_equal("first:4", set%get_first(4), 4)
-  call assert_equal("last:1", set%get_last(1), 2)
-  call assert_equal("last:2", set%get_last(2), 3)
-  call assert_equal("last:4", set%get_last(4), 6)
-  call assert_equal("length:1", set%get_length(1), 2)
-  call assert_equal("length:2", set%get_length(2), 1)
-  call assert_equal("length:3", set%get_length(3), 0)
-  call assert_equal("length:4", set%get_length(4), 3)
+  
+  call start_tests("get_first")
+  call add_test("index=1", set%get_first(1), 1)
+  call add_test("index=2", set%get_first(2), 3)
+  call add_test("index=4", set%get_first(4), 4)
+  call end_tests()
+  
+  call start_tests("get_last")
+  call add_test("index=1", set%get_last(1), 2)
+  call add_test("index=2", set%get_last(2), 3)
+  call add_test("index=4", set%get_last(4), 6)
+  call end_tests()
+
+  call start_tests("get_length")
+  call add_test("index=1", set%get_length(1), 2)
+  call add_test("index=2", set%get_length(2), 1)
+  call add_test("index=3", set%get_length(3), 0)
+  call add_test("index=4", set%get_length(4), 3)
+  call end_tests()
   
 
 end program
