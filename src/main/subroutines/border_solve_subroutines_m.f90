5f8dc605fad7509ca857fa7c49b19f9c9474895e
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-04 19:37:50 +0900

    add border_update

--- src/main/subroutines/border_solve_subroutines_m.f90
+++ src/main/subroutines/border_solve_subroutines_m.f90
@@ -1,11 +1,12 @@
 module border_solve_subroutines_m
   use border_solve_kernel_m
+  use solve_kernel_m
   use factors_m
   use right_hand_m
   implicit none
   private
 
-  public :: border_forward, border_backward
+  public :: border_forward, border_update_l, border_backward, border_update_u
 
 contains
   subroutine border_forward(factors, rh, node, j)
@@ -24,6 +25,22 @@ contains
     
   end subroutine
 
+  subroutine border_update_l(factors, rh, node, i, j)
+    type(factors_c), pointer :: factors
+    type(right_hand_c), pointer :: rh
+    integer, intent(in) :: node, i, j
+    double precision, pointer, contiguous :: a(:), b1(:), b2(:)
+    integer :: ncol, nrow
+
+    a => factors%get_supernode_ptr(node, i, j)
+    b1 => rh%get_supernode_ptr(node, j)
+    b2 => rh%get_array_ptr(node, i)
+    ncol = factors%get_supernode_block_size(j, node)
+    nrow = factors%get_block_size(i, node)
+    call mydgemv_t(a, ncol, nrow, b1, b2)
+
+  end subroutine
+
   subroutine border_backward(factors, rh, node, j)
     type(factors_c), pointer :: factors
     type(right_hand_c), pointer :: rh
@@ -38,6 +55,22 @@ contains
     b2 => rh%get_work_ptr(node, j)
     call border_dtrsv_u(a, b1, b2, ncol, nrow-ncol)
     
+  end subroutine
+
+  subroutine border_update_u(factors, rh, node, i, j)
+    type(factors_c), pointer :: factors
+    type(right_hand_c), pointer :: rh
+    integer, intent(in) :: node, i, j
+    integer :: ncol, nrow
+    double precision, pointer, contiguous :: a(:), b1(:), b2(:)
+    
+    a => factors%get_supernode_ptr(node, i, j)
+    b1 => rh%get_array_ptr(node, i)
+    b2 => rh%get_supernode_ptr(node, j)
+    ncol = factors%get_supernode_block_size(j, node)
+    nrow = factors%get_block_size(i, node)
+    call mydgemv_n(a, ncol, nrow, b1, b2)
+
   end subroutine
 
 end module
--- src/test/subroutines/border_solve_subroutines_test.f90
+++ src/test/subroutines/border_solve_subroutines_test.f90
@@ -16,23 +16,31 @@ program border_solve_subroutines_test
   integer :: nb
 
   nb = 5
-  node_data => create_node_data([3,2],[2,0],nb)
-  node_sets => create_contiguous_sets([3,2])
-  ccs => create_jagged_array([2,0])
+  node_data => create_node_data([3,6],[6,0],nb)
+  node_sets => create_contiguous_sets([3,6])
+  ccs => create_jagged_array([6,0])
   factors => create_factors(node_data, node_sets, ccs, nb)
   rh => create_right_hand(node_data, node_sets, ccs, nb)
 
   factors%get_supernode_ptr(1,1,1) = [4d0,0d0,0d0,3d0,3d0,0d0,2d0,2d0,2d0,1d0,1d0,1d0,2d0,2d0,2d0]
   rh%get_supernode_ptr(1,1) = [4d0,6d0,6d0]
   rh%get_work_ptr(1,1) = [3d0,6d0]
+  factors%get_supernode_ptr(1,2,1) = [1,1,1,2,2,2,3,3,3,4,4,4]
+  rh%get_work_ptr(1,2) = [3,6,9,12]
   call border_forward(factors, rh, 1, 1)
   call assert_equal("border_forward", [rh%get_supernode_ptr(1,1), rh%get_work_ptr(1,1)], [1d0,1d0,1d0,0d0,0d0])
-  
-  factors%get_supernode_ptr(1,1,1) = [2d0,0d0,0d0,2d0,3d0,0d0,2d0,3d0,4d0,1d0,2d0,3d0,1d0,2d0,3d0]
-  rh%get_supernode_ptr(1,1) = [9d0,12d0,13d0]
-  rh%get_work_ptr(1,1) = [1d0,2d0]
+  call border_update_l(factors, rh, 1, 2, 1)
+  call assert_equal("border_update_l", rh%get_work_ptr(2,1), [0d0,0d0,0d0,0d0,0d0])
 
+  factors%get_supernode_ptr(1,1,1) = [2d0,0d0,0d0,2d0,3d0,0d0,2d0,3d0,4d0,1d0,2d0,3d0,1d0,2d0,3d0]
+  factors%get_supernode_ptr(1,2,1) = [1,2,3,1,2,3,1,2,3,1,2,3]
+  rh%get_supernode_ptr(1,1) = [9d0+10d0,12d0+20d0,13d0+30d0]
+  rh%get_work_ptr(1,1) = [1d0,2d0]
+  rh%get_work_ptr(1,2) = [1,2,3,4]
+
+  call border_update_u(factors, rh, 1, 2, 1)
+  call assert_equal("border_update_u", rh%get_supernode_ptr(1,1), [9d0,12d0,13d0])
   call border_backward(factors, rh, 1, 1)
-  call assert_equal("border_backward", [rh%get_supernode_ptr(1,1), rh%get_work_ptr(1,1)], [1d0,1d0,1d0,1d0,2d0])
+  call assert_equal("border_backward", rh%get_supernode_ptr(1,1), [1d0,1d0,1d0])
   
 end program
