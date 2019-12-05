c05df1e1b18c5f5ded0689e8e4df5f35c2efa0d9
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-05 15:44:02 +0900

    modify scatter_add_test

--- src/test/subroutines/sparse_add_subroutines_test.f90
+++ src/test/subroutines/sparse_add_subroutines_test.f90
@@ -24,27 +24,27 @@ program sparse_add_subroutines_test
   block_local_index => create_block_local_index(node_data, local_index)
   rh => create_right_hand(node_data, node_set, local_index, 3)
   
-  rh%get_work_ptr(1,2) = 1d0
-  rh%get_work_ptr(1,3) = 1d0
-  rh%get_work_ptr(1,4) = 1d0
-  rh%get_array_ptr(2,1) = 1d0
-  rh%get_array_ptr(2,2) = 1d0
-  rh%get_array_ptr(2,3) = 1d0
-  rh%get_array_ptr(2,4) = 1d0
+  rh%get_work_ptr(1,2) = [1,2]
+  rh%get_work_ptr(1,3) = [3,4,5]
+  rh%get_work_ptr(1,4) = [6,7]
+  rh%get_array_ptr(2,1) = 0d0
+  rh%get_array_ptr(2,2) = 0d0
+  rh%get_array_ptr(2,3) = 0d0
+  rh%get_array_ptr(2,4) = 0d0
   
   call start_array_tests("scatter_add")
   call scatter_add(factors, rh, block_local_index, 1, 1, 2)
-  call add_test("node=1, index=1", rh%get_array_ptr(2, 1), [1d0,1d0,2d0])
+  call add_test("node=1, index=1", rh%get_array_ptr(2, 1), [0d0,0d0,1d0])
   call scatter_add(factors, rh, block_local_index, 2, 1, 2)
-  call add_test("node=1, index=2", rh%get_array_ptr(2, 2), [2d0,1d0,1d0])
+  call add_test("node=1, index=2", rh%get_array_ptr(2, 2), [2d0,0d0,0d0])
   call scatter_add(factors, rh, block_local_index, 3, 1, 2)
-  call add_test("node=1, index=3", rh%get_array_ptr(2, 2), [2d0,2d0,2d0])
+  call add_test("node=1, index=3", rh%get_array_ptr(2, 2), [2d0,3d0,4d0])
   call scatter_add(factors, rh, block_local_index, 4, 1, 2)
-  call add_test("node=1, index=4", rh%get_array_ptr(2, 3), [1d0,2d0,1d0])
+  call add_test("node=1, index=4", rh%get_array_ptr(2, 3), [0d0,5d0,0d0])
   call scatter_add(factors, rh, block_local_index, 5, 1, 2)
-  call add_test("node=1, index=5", rh%get_array_ptr(2, 3), [1d0,2d0,2d0])
+  call add_test("node=1, index=5", rh%get_array_ptr(2, 3), [0d0,5d0,6d0])
   call scatter_add(factors, rh, block_local_index, 6, 1, 2)
-  call add_test("node=1, index=6", rh%get_array_ptr(2, 4), [2d0])
+  call add_test("node=1, index=6", rh%get_array_ptr(2, 4), [7d0])
   call end_array_tests()
   
   rh%get_work_ptr(1,2) = 0d0
