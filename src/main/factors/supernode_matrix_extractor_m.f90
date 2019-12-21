c31d5d6dbb6148eacd16ff77c4316cbc213f6f18
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-21 19:57:06 +0900

    supernode_matrix_extractorのリファクタリング

--- src/main/factors/supernode_matrix_extractor_m.f90
+++ src/main/factors/supernode_matrix_extractor_m.f90
@@ -15,14 +15,15 @@ contains
   integer function get_start_pos(this, node, i, j) result(pos)
     class(supernode_extractor_c) :: this
     integer, intent(in) :: node, i, j
-    integer :: left, up, nb, nc, nr
+    integer :: n, nb, col_size
+    integer :: left, up
 
+    n = this%node_data%supernode_size(node) + this%node_data%work_size(node)
     nb = this%node_data%nb
-    nc = this%node_data%supernode_size(node)
-    nr = this%node_data%work_size(node)
+    col_size = this%node_data%get_supernode_block_size(j, node)
 
-    left = partial_sum(nc+nr-(j-1)*nb+1, nc+nr) + partial_sum(nb-1)*(j-1)
-    up = (i-j)*nb*get_block_size(j, nb, nc)
+    left = partial_sum(n-(j-1)*nb+1, n) + partial_sum(nb-1)*(j-1)
+    up = (i-j)*nb*col_size
     pos = left + up + 1
 
   end function
@@ -30,28 +31,24 @@ contains
   integer function get_size(this, node, i, j) result(supernode_size)
     class(supernode_extractor_c) :: this
     integer, intent(in) :: node, i, j
-    integer :: nb, nc, nr
+    integer :: col_size, row_size
     
-    
-    nb = this%node_data%nb
-    nc = this%node_data%supernode_size(node)
-    nr = this%node_data%work_size(node)
-    supernode_size = get_block_size(i, nb, nc+nr) * get_block_size(j, nb, nc)
+    row_size = this%node_data%get_matrix_block_size(i, node)
+    col_size = this%node_data%get_supernode_block_size(j, node)
+    supernode_size = row_size*col_size
 
   end function
 
   integer function estimate_size(this, node) result(supernode_size)
     class(supernode_extractor_c) :: this
     integer, intent(in) :: node
-    integer :: nc, nr, nb
-    integer :: sn, sr
+    integer :: nc, nr, nb, sn, sr
 
     nb = this%node_data%nb
     nc = this%node_data%supernode_size(node)
     nr = this%node_data%work_size(node)
-
     sn = nc/nb
-    sr = mod(nc, nb)
+    sr = this%node_data%border_supernode_size(node)
     
     supernode_size = partial_sum(nr+1, nc+nr) + partial_sum(nb-1)*sn+partial_sum(sr-1)
     
