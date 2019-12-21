215578b87ab5537b730bcc2e814719b7d7d0ad98
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-21 20:11:16 +0900

    border_matrix_extractorのリファクタリング

--- src/main/factors/border_matrix_extractor_m.f90
+++ src/main/factors/border_matrix_extractor_m.f90
@@ -14,11 +14,9 @@ contains
   integer function get_start_pos(this, node, i, j) result(pos)
     class(border_extractor_c) :: this
     integer, intent(in) :: node, i, j
-    integer :: nb, nc, nr
+    integer :: nb
 
     nb = this%node_data%nb
-    nc = this%node_data%supernode_size(node)
-    nr = this%node_data%work_size(node)
     pos = (i-j)*nb*nb+1
 
   end function
@@ -26,13 +24,11 @@ contains
   integer function get_size(this, node, i, j) result(border_size)
     class(border_extractor_c) :: this
     integer, intent(in) :: node, i, j
-    integer :: nb, nc, nr
+    integer :: col_size, row_size
 
-    nb = this%node_data%nb
-    nc = this%node_data%supernode_size(node)
-    nr = this%node_data%work_size(node)
-    
-    border_size = get_block_size(j, nb, nc+nr)*get_block_size(i, nb, nc+nr)
+    col_size = this%node_data%get_matrix_block_size(j, node)
+    row_size = this%node_data%get_matrix_block_size(i, node)
+    border_size = col_size * row_size
     
   end function
 
@@ -40,31 +36,21 @@ contains
     class(border_extractor_c) :: this
     integer, intent(in) :: node
     integer :: nc, nr, nb
-    integer :: width, sr
+    integer :: row_size, j, col_size
 
     nb = this%node_data%nb
     nc = this%node_data%supernode_size(node)
     nr = this%node_data%work_size(node)
 
-    sr = mod(nc, nb)
-
-    if(mod(nc, nb) == 0)then
+    if(this%node_data%divisible(node))then
       border_size = 0
-    else
-      if(nc+nr < nb)then
-        width = nc+nr
-        if(width == 0)then
-          width = nb
-        endif
-        border_size = width*width
-      else
-        if(nr < nb-sr)then
-          border_size = (nc+nr-(nc/nb)*nb)*(sr+nr)
-        else
-          border_size = (nc+nr-(nc/nb)*nb)*nb
-        endif
-      endif
+      return
     endif
+    
+    j = this%node_data%get_work_start_index(node)
+    col_size = this%node_data%get_matrix_block_size(j, node)
+    row_size = nc+nr-(nc/nb)*nb
+    border_size = row_size*col_size
     
   end function
 end module
