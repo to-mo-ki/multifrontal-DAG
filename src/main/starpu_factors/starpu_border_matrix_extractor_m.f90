99e0766c0776a85a0fba605cbb53ae688ab6a199
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-26 02:11:58 +0900

    border_matrix_extractorの実装

--- src/main/starpu_factors/starpu_border_matrix_extractor_m.f90
+++ src/main/starpu_factors/starpu_border_matrix_extractor_m.f90
@@ -12,13 +12,22 @@ contains
   integer function get_pos(this, node, i, j) result(ptr)
     class(border_extractor_c) :: this
     integer, intent(in) :: node, i, j
+
+    ptr = i - j + 1
     
   end function
 
   integer function estimate_size(this, node)
     class(border_extractor_c) :: this
     integer, intent(in) :: node
-
+    integer :: num_block, border_index
+    if(this%node_data%divisible(node))then
+      estimate_size = 0
+      return
+    endif
+    num_block = this%node_data%get_num_matrix_block(node)
+    border_index = this%node_data%get_work_start_index(node)
+    estimate_size = num_block - border_index + 1
 
   end function
 end module
