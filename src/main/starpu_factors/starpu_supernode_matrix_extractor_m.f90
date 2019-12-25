748243af88e897285204577017b0a3c45b53d4a5
Tomoki Nakano <tomoki_nakano@stu.kobe-u.ac.jp>
2019-12-26 02:32:17 +0900

    supernode_extractorの実装

--- src/main/starpu_factors/starpu_supernode_matrix_extractor_m.f90
+++ src/main/starpu_factors/starpu_supernode_matrix_extractor_m.f90
@@ -14,12 +14,26 @@ contains
   integer function get_pos(this, node, i, j) result(ptr)
     class(supernode_extractor_c) :: this
     integer, intent(in) :: node, i, j
+    integer :: num_block
+
+    num_block = this%node_data%get_num_matrix_block(node)
+    ptr = triangular_pos(i, j, num_block)
     
   end function
 
   integer function estimate_size(this, node)
     class(supernode_extractor_c) :: this
     integer, intent(in) :: node
+    integer :: num_matrix, last, num_last
     
+    num_matrix = this%node_data%get_num_matrix_block(node)
+    if(this%node_data%divisible(node))then
+      last = this%node_data%get_work_start_index(node) - 1
+    else
+      last = this%node_data%get_work_start_index(node)
+    endif
+    num_last = num_matrix - last + 1
+    estimate_size = partial_sum(num_last, num_matrix)
+
   end function
 end module
